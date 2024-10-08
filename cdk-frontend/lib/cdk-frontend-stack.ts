0f140d33916dca42a8f2cc7b00d9b700951c885c
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-10-07 23:37:28 -0500

    add cdk-frontend

---
+++ cdk-frontend/lib/cdk-frontend-stack.ts
@@ -0,0 +1,77 @@
+import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {
+  Bucket,
+  BucketAccessControl,
+  BlockPublicAccess,
+} from 'aws-cdk-lib/aws-s3';
+import {
+  Distribution,
+  OriginAccessIdentity,
+  ViewerProtocolPolicy,
+  AllowedMethods,
+  CachedMethods,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
+
+export class CdkFrontendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket for the website
+    const websiteBucket = new Bucket(this, 'WebsiteBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      accessControl: BucketAccessControl.PRIVATE,
+      removalPolicy: RemovalPolicy.DESTROY, // Change to RETAIN for production
+      autoDeleteObjects: true, // Be careful with this in production
+    });
+
+    // Create an Origin Access Identity for CloudFront
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity'
+    );
+    websiteBucket.grantRead(originAccessIdentity);
+
+    // Create a CloudFront distribution
+    const distribution = new Distribution(this, 'WebsiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(websiteBucket, {
+          originAccessIdentity: originAccessIdentity,
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachedMethods: CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      },
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(5),
+        },
+      ],
+    });
+
+    // Deploy the Angular build output to the S3 bucket
+    new BucketDeployment(this, 'DeployWebsite', {
+      sources: [
+        Source.asset(
+          path.resolve(__dirname, '../../FrontendAngular/dist/frontend-angular')
+        ),
+      ],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // Output the CloudFront distribution domain name
+    new CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.domainName,
+    });
+  }
+}
--- cdk-frontend/test/cdk-frontend.test.ts
+++ cdk-frontend/test/cdk-frontend.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkInfra from '../lib/cdk-infra-stack';
+// import * as CdkFrontend from '../lib/cdk-frontend-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-infra-stack.ts
+// example resource in lib/cdk-frontend-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new CdkInfra.CdkInfraStack(app, 'MyTestStack');
+//   const stack = new CdkFrontend.CdkFrontendStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
