272237948ff803cf3a86c0432a1b9cf55e881173
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-10-08 19:02:57 -0500

    feat: change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Demo Daily';
+  title = 'Github Actions CI/CD with AWS';
 }
