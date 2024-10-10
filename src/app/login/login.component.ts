c8c0123f49a274ef41c7579221a8f23c3f1d631b
Oscar Giovanni Fonseca Neira <o.fonsenan@uniandes.edu.co>
2024-10-10 02:05:36 -0500

    feature: Add custom theme with palette

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,15 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
