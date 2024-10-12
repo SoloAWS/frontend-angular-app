316935f9328cbfc92b7f9366fb27d50ec8f7de9a
Oscar Giovanni Fonseca Neira <o.fonsenan@uniandes.edu.co>
2024-10-11 19:03:15 -0500

    feature: create company service

---
+++ src/app/signup/company.ts
@@ -0,0 +1,36 @@
+export class Company {
+  id: number;
+  name: string;
+  fist_name: string;
+  last_name: string;
+  birth_date: string;
+  phone_number: string;
+  username: string;
+  country: string;
+  city: string;
+  password: string;
+
+  constructor(
+    id: number,
+    name: string,
+    fist_name: string,
+    last_name: string,
+    birth_date: string,
+    phone_number: string,
+    username: string,
+    country: string,
+    city: string,
+    password: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.fist_name = fist_name;
+    this.last_name = last_name;
+    this.birth_date = birth_date;
+    this.phone_number = phone_number;
+    this.username = username;
+    this.country = country;
+    this.city = city;
+    this.password = password;
+  }
+}
---
+++ src/app/signup/signup.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Company } from './company';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  private apiUrl = "localhost:8001";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  crearCompany(company: Company): Observable<Company> {
+    return this.http.post<Company>(`${this.apiUrl}/company`, company)
+  }
+}
