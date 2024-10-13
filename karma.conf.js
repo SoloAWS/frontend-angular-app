b56609ae48de77a5aa81ef590a137081828c4bb1
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2024-10-13 17:10:21 -0500

    feat: added karma file config

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,39 @@
-const path = require('path');
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      'karma-jasmine',
-      'karma-chrome-launcher',
-      'karma-coverage',
-      '@angular-devkit/build-angular/plugins/karma'
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    preprocessors: {
-      './src/**/*.ts': ['coverage'] // Only process TypeScript files for coverage
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/'),
+      dir: require('path').join(__dirname, './coverage/frontend-angular'),
       subdir: '.',
       reporters: [
-        { type: 'html' }, // Generates an HTML report
-        { type: 'lcov', subdir: '.' }, // Generates lcov.info
-        { type: 'text-summary' } // Summary in console
+        { type: 'html' },
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'coverage'], // You may omit coveralls unless you need it
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['ChromeHeadless'],
-    singleRun: true,
-    concurrency: Infinity
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
   });
 };
