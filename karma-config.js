16250b38e29fd119926afa87b938fa9213e53de8
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2024-10-13 16:11:05 -0500

    fix: second try for github action

--- karma-config.js
+++ karma-config.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov' }
+        { type: 'lcovonly' }
       ]
     },
     reporters: ['progress', 'kjhtml', 'coverage'],
