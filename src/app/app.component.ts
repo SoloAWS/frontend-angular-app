fd7187a2609a900b3bed8fa0608b1bfcc536c474
ofonsecan <o.fonsecan@uniandes.edu.co>
2024-10-17 00:05:27 -0500

    feature: Add menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(public router: Router) { }
+
   title = 'New stack';
+
+  isSidenavOpened = true;
+
+  toggleSidenav() {
+    this.isSidenavOpened = !this.isSidenavOpened;
+  }
+
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
 }
