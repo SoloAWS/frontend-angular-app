b2a5eaf46fe7bd4512294548297c41f34548931f
ofonsecan <o.fonsecan@uniandes.edu.co>
2024-10-18 14:20:18 -0500

    feature: create dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { IncidentCreateComponent } from './incident/incident-create/incident-create.component';
+import { DashboardComponent } from './incident/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'incident/dashboard', component: DashboardComponent },
     { path: 'incident/create', component: IncidentCreateComponent },
 ];
--- src/app/incident/incident-create/incident-create.component.ts
+++ src/app/incident/incident-create/incident-create.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-incident-create',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './incident-create.component.html',
   styleUrl: './incident-create.component.css'
 })
