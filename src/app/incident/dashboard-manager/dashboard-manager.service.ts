db1eb9f94e620b51f47f75f0ac86780c44bebbfd
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-11-07 22:11:12 -0500

    feat: use api endpoints

--- src/app/incident/dashboard-manager/dashboard-manager.service.ts
+++ src/app/incident/dashboard-manager/dashboard-manager.service.ts
@@ -1,66 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { IncidentList, DailyStats } from '../../models';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
   getPrioritizedIncidents(): Observable<IncidentList[]> {
-    return of([
-      {
-        id: 'ID-004',
-        description: 'Interrupción de servicio',
-        state: 'En Progreso',
-        channel: 'Móvil',
-        priority: 'Alta',
-        creation_date: '2024-12-09T15:00:00',
-        user_id: 'U123',
-        company_id: 'C001',
-        company_name: 'Cliente VIP',
-        manager_id: null,
-      },
-    ]);
-  }
-
-  getAssignedIncidents(): Observable<IncidentList[]> {
-    return of([
-      {
-        id: 'ID-001',
-        description: 'Problema de Conexión',
-        state: 'En Proceso',
-        channel: 'Correo',
-        priority: 'Media',
-        creation_date: '2024-12-09T15:00:00',
-        user_id: 'U124',
-        company_id: 'C002',
-        company_name: 'Cliente A',
-        manager_id: 'M001',
-      },
-    ]);
-  }
-
-  getDailyStats(): Observable<DailyStats> {
-    return of({
-      incidentsHandled: 15,
-      avgResolutionTime: '12 mins',
-      customerSatisfaction: 4.5,
-    });
-  }
-
-  /*  getPrioritizedIncidents(): Observable<IncidentList[]> {
     return this.http.get<IncidentList[]>(
-      `${this.apiUrl}/report-generation/user/prioritized-incidents`
+      `${this.apiUrl}/incident-management/manager/high-priority-assigned-incidents`
     );
   }
 
   getAssignedIncidents(): Observable<IncidentList[]> {
     return this.http.get<IncidentList[]>(
-      `${this.apiUrl}/report-generation/user/assigned-incidents`
+      `${this.apiUrl}/incident-management/manager/assigned-incidents`
     );
   }
 
   getDailyStats(): Observable<DailyStats> {
-    return this.http.get<DailyStats>(`${this.apiUrl}/report-generation/user/daily-stats`);
-  } */
+    return this.http.get<DailyStats>(
+      `${this.apiUrl}/incident-management/manager/daily-stats`
+    );
+  }
 }
